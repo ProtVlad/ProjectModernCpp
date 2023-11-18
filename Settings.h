ba5f5bec36f0aefda76cb123ef01aa87c6c20f45
zincaI <zinca.irina13@gmail.com>
2023-11-18 19:39:53 +0200

    Corrected setters and added definitions

--- Settings.h
+++ Settings.h
@@ -18,8 +18,17 @@ public:
 	void setNumberPlayers(uint16_t maxNumberPlayers);
 	void setMaxClues(uint16_t maxClues);
 	void setDrawtime(uint16_t drawtime);
-	void setNumberWods();
-	void setNumberHints();
+	//ai doua randuri din corectatea setterilor pt saptamana viitoare+3 randuri din functii=>1 commit
+	void setNumberWods(uint16_t numberWords);
+	void setNumberHints(uint16_t numberHints);
+	void optionsTime();
+	void optionsNumberPlayers();
+	void optionsNumberRounds();
+	void optionsNumberChosenWords();
+	//optionHints de dezbatut
+	//void optionsHints(uint16_t maxHints);
+	// de dezbatut daca e nevoie
+	//void choosePublicOrPrivate();
 	
 
 private:
@@ -29,7 +38,9 @@ private:
 	uint16_t m_drawtime;
 	uint16_t m_maxClues;
 	uint16_t m_numberWords;
-	uint16_t m_numberHints;
+	//uint16_t m_numberHints;
+	//bool optionPrivate;
+	uint16_t m_chosenWords;
 	
 };
 
