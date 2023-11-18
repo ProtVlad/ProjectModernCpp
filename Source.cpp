67f9376aa4f472bc85fec8e96f92b5cffeefc6b8
zincaI <zinca.irina13@gmail.com>
2023-11-18 19:28:15 +0200

    Added port to the Source in ProjectModernCpp

--- Source.cpp
+++ Source.cpp
@@ -3,7 +3,15 @@
 #include "Player.h"
 #include "Settings.h"
 
+#include <crow.h>
+
+
 int main()
 {
+	crow::SimpleApp app;
+	CROW_ROUTE(app, "/")([]() {
+		return "This is the path";
+		});
+	app.port(13034).multithreaded().run();
 	return 0;
 }
