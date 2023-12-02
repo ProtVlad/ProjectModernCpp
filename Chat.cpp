2b1a86bd15a5ec9333eba2036c86e92cf42634cc
sgeorgianai <147824842+sgeorgianai@users.noreply.github.com>
2023-12-02 13:09:06 +0200

    Modify Chat class

--- Chat.cpp
+++ Chat.cpp
@@ -6,36 +6,11 @@ Chat::Chat()
 	//EMPTY
 }
 
-Chat::Chat(std::string guess, std::vector<std::pair<Player, std::string>> playerActivity,
-	bool right, bool close, std::vector<Player> playerList, std::string rightWord) :
-	m_guess(guess),
-	m_playerActivity(playerActivity),
-	m_right(right),
-	m_close(close),
-	m_playerList(playerList),
-	m_rightWord(rightWord)
-{
-}
-
-const std::string& Chat::getGuess()
-{
-	return m_guess;
-}
-
-const std::vector<Player>& Chat::getPlayerList()
+const std::vector<Player>& Chat::GetPlayerList()
 {
 	return m_playerList;
 }
 
-const std::string& Chat::getRightWord()
-{
-	return m_rightWord;
-}
-
-void Chat::setPlayerActivity(std::vector<std::pair<Player, std::string>> playerActivity)
-{
-	m_playerActivity = playerActivity;
-}
 
 void Chat::ShowOpinion(Player player)
 {
@@ -45,34 +20,11 @@ void Chat::ShowOpinion(Player player)
 		std::cout << player.getName() << " disliked the drawing";
 }
 
-void Chat::ShowGuess(Player player, std::string guess)
-{
-	std::cout << player.getName() << ": " << guess;
-}
 
-bool Chat::isClose()
-{
-	return m_close;
-}
-
-bool Chat::isRight()
-{
-	return m_right;
-}
 
 Chat::~Chat()
 {
-	m_playerActivity.clear();
 	m_playerList.clear();
 }
 
-int Chat::calculateScore(Player player, uint16_t maxTime)
-{
-	if (isRight() && player.getAnswerTime() <= maxTime / 2)
-		return 100;
-	if (isRight() && player.getAnswerTime() > maxTime / 2)
-		return ((maxTime - player.getAnswerTime()) * 100) / (maxTime / 2);
-	if (player.getAnswerTime() == maxTime)
-		return -50;
-}
 
--- Chat.h
+++ Chat.h
@@ -9,23 +9,11 @@ class Chat
 
 public:
 	Chat();
-	Chat(std::string guess, std::vector<std::pair<Player, std::string>> playerActivity,
-		bool right, bool close, std::vector<Player> playerList, std::string rightWord);
-	const std::string& getGuess();
-	const std::vector<Player>& getPlayerList();
-	const std::string& getRightWord();
-	void setPlayerActivity(std::vector<std::pair<Player, std::string>> playerActivity);
+	const std::vector<Player>& GetPlayerList();
 	void ShowOpinion(Player player);
-	void ShowGuess(Player player, std::string guess);
-	bool isClose();
-	bool isRight();
 	~Chat();
-	int calculateScore(Player player, uint16_t maxTime);
 
 private:
-	std::string m_guess;
-	std::vector<std::pair<Player, std::string>> m_playerActivity;
-	bool m_right, m_close;
 	std::vector<Player> m_playerList;
-	std::string m_rightWord;
+
 };
