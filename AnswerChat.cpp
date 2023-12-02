baeea944cb18fcc9b69b861a26c680785f032fd0
sgeorgianai <147824842+sgeorgianai@users.noreply.github.com>
2023-12-02 13:10:38 +0200

    Added AnswerChat cpp

---
+++ AnswerChat.cpp
@@ -0,0 +1,76 @@
+#include "AnswerChat.h"
+#include "Player.h"
+
+AnswerChat::AnswerChat()
+{
+	//EMPTY
+}
+
+AnswerChat::AnswerChat(std::string guess, bool isDrawing,
+	bool right, bool close, std::vector<Player> playerList, std::string rightWord) :
+	m_guess(guess),
+	m_isDrawing(isDrawing),
+	m_right(right),
+	m_close(close),
+	m_playerList(playerList),
+	m_rightWord(rightWord)
+{
+}
+
+const std::string& AnswerChat::GetGuess()
+{
+	return m_guess;
+}
+
+const std::vector<Player>& AnswerChat::GetPlayerList()
+{
+	return m_playerList;
+}
+
+const std::string& AnswerChat::GetRightWord()
+{
+	return m_rightWord;
+}
+
+void AnswerChat::SetIsDrawing(bool isDrawing)
+{
+	m_isDrawing = isDrawing;
+}
+
+void AnswerChat::ShowOpinion(Player player)
+{
+	if (player.getOpinion() == ConvertStringToOpinion("Like"))
+		std::cout << player.getName() << " liked the drawing";
+	if (player.getOpinion() == ConvertStringToOpinion("Dislike"))
+		std::cout << player.getName() << " disliked the drawing";
+}
+
+void AnswerChat::ShowGuess(Player player, std::string guess)
+{
+	std::cout << player.getName() << ": " << guess;
+}
+
+bool AnswerChat::IsClose()
+{
+	return m_close;
+}
+
+bool AnswerChat::IsRight()
+{
+	return m_right;
+}
+
+AnswerChat::~AnswerChat()
+{
+	m_playerList.clear();
+}
+
+int AnswerChat::CalculateScore(Player player, uint16_t maxTime)
+{
+	if (IsRight() && player.getAnswerTime() <= maxTime / 2)
+		return 100;
+	if (IsRight() && player.getAnswerTime() > maxTime / 2)
+		return ((maxTime - player.getAnswerTime()) * 100) / (maxTime / 2);
+	if (player.getAnswerTime() == maxTime)
+		return -50;
+}
