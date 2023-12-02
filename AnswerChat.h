518eee1f41542930bd959b91b090eb8ff0044943
sgeorgianai <147824842+sgeorgianai@users.noreply.github.com>
2023-12-02 13:10:13 +0200

    Added AnswerChat header

---
+++ AnswerChat.h
@@ -0,0 +1,28 @@
+#include <vector>
+#include "Player.h"
+
+#pragma once
+class AnswerChat
+{
+
+public:
+	AnswerChat();
+	AnswerChat(std::string guess, bool isDrawing,
+		bool right, bool close, std::vector<Player> playerList, std::string rightWord);
+	const std::string& GetGuess();
+	const std::vector<Player>& GetPlayerList();
+	const std::string& GetRightWord();
+	void SetIsDrawing(bool isDrawing);
+	void ShowOpinion(Player player);
+	void ShowGuess(Player player, std::string guess);
+	bool IsClose();
+	bool IsRight();
+	~AnswerChat();
+	int CalculateScore(Player player, uint16_t maxTime);
+
+private:
+	std::string m_guess;
+	bool m_right, m_close, m_isDrawing;
+	std::vector<Player> m_playerList;
+	std::string m_rightWord;
+};
