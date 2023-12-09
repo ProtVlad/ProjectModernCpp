e85b283f727d9d2e865a73104f5224434b733378
zincaI <zinca.irina13@gmail.com>
2023-12-09 15:38:01 +0200

    Server connection with GUI

--- GUI/main.cpp
+++ GUI/main.cpp
@@ -1,9 +1,23 @@
+#include <iostream>
+#include <cpr/cpr.h>
+#include <crow.h>
+
 #include "mainwindow.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
+
+    cpr::Response response = cpr::Get(cpr::Url{ "http://localhost:13034/products" });
+
+    if (response.error) {
+        std::cerr << "Eroare la conectarea la server: " << response.error.message << std::endl;
+    }
+    else {
+        std::cout << "Raspuns de la server: " << response.text << std::endl;
+    }
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
