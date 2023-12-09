66ba29b3d7a3a685729ee563e5e0a0f5692f2916
zincaI <zinca.irina13@gmail.com>
2023-12-10 00:57:07 +0200

    Connected server to the database

--- Source.cpp
+++ Source.cpp
@@ -1,13 +1,25 @@
-#include "Chat.h"
+﻿#include "Chat.h"
 #include "Game.h"
 #include "Player.h"
 #include "Settings.h"
 
 #include <crow.h>
-
+#include <libpq-fe.h>
 
 int main()
 {
+
+    PGconn* conn = PQconnectdb("host=localhost user=postgres password=1q2w3e dbname=romanian_words");
+
+    if (PQstatus(conn) == CONNECTION_OK) {
+        std::cout << "Conectat la baza de date PostgreSQL!\n";
+     //ag=daugare interogari
+        PQfinish(conn);
+    }
+    else {
+        std::cerr << "Eroare de conectare la baza de date PostgreSQL: " << PQerrorMessage(conn) << "\n";
+    }
+
 	crow::SimpleApp app;
 	CROW_ROUTE(app, "/")([]() {
 		return "This is the path";
